dab68f6a849f584e9b6e8c0f9b509ccf01dc9776
TONY2\creba <creba1234@gmail.com>
2023-12-15 15:01:20 +0900

    인프런 코테강의 03-01 완료

--- Algorithm-test/src/inflearn/basicTest/ch3_TwoPointers_SlidingWindows/CombiningTwoArrays.java
+++ Algorithm-test/src/inflearn/basicTest/ch3_TwoPointers_SlidingWindows/CombiningTwoArrays.java
@@ -1,27 +1,18 @@
-package inflearn.basicTest.template;
-
-import inflearn.basicTest.ch2_Array.Mountaintop;
+package inflearn.basicTest.ch3_TwoPointers_SlidingWindows;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class CombiningTwoArrays {
 
     public ArrayList<Integer> solution(int n, int m, int[] arr1, int[] arr2) {
         ArrayList<Integer> answer = new ArrayList<>();
-        int p1=0, p2=0;
-        while(p1<n && p2<m){
-            if(arr1[p1]<arr2[p2]) answer.add(arr1[p1++]); //add하고, 증가
-            else answer.add(arr2[p2++]);
-        }
-        while(p1<n) answer.add(arr1[p1++]);
-        while(p2<m) answer.add(arr2[p2++]);
 
         return answer;
     }
 
     public static void main(String[] args) {
-        Main main = new Main();
+        CombiningTwoArrays main = new CombiningTwoArrays();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr1 = new int[n];
