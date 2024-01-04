cfe2679eb8963889ed8009494de903d77f7a8466
TONY2\creba <creba1234@gmail.com>
2024-01-04 19:43:03 +0900

    인프런 코테강의 06-03 완료

---
+++ Algorithm-test/src/inflearn/basicTest/ch6_SortingAndSearching/InsertionSort.java
@@ -0,0 +1,32 @@
+package inflearn.basicTest.ch6_SortingAndSearching;
+
+import java.util.Scanner;
+
+public class InsertionSort {
+
+    public int[] solution(int n, int[] arr) {
+        int[] answer = arr;
+        for(int i = 1; i < n; i++){
+            int tmp = arr[i], j;
+            for(j = i-1; j >= 0; j--){
+                if(arr[j] > tmp) arr[j+1] = arr[j];
+                else break;
+            }
+            arr[j+1] = tmp;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        InsertionSort main = new InsertionSort();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i]= sc.nextInt();
+        }
+        for(int i : main.solution(n, arr)){
+            System.out.print(i + " ");
+        }
+    }
+}
