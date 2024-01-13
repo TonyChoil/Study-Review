4be320380fdf6ecd254abb6fdaec5316c6b24b8a
TONY2\creba <creba1234@gmail.com>
2024-01-13 11:05:51 +0900

    인프런 코테강의 07-11/12 완료

---
+++ Algorithm-test/src/inflearn/basicTest/ch7_Resursive_Tree_Graph/DFS4.java
@@ -0,0 +1,39 @@
+package inflearn.basicTest.ch7_Resursive_Tree_Graph;
+
+import java.util.Scanner;
+
+public class DFS4 {
+    static int n, m, answer;
+    static int[][] graph;
+    static int[] ch;
+    public void DFS(int v){
+        if(v==n) answer++;
+        else{
+            for(int i = 1; i <= n; i++){
+                if(graph[v][i] == 1 && ch[i] == 0){
+                    ch[i] = 1;
+                    DFS(i);
+                    ch[i] = 0;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        DFS4 T = new DFS4();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        graph = new int [n+1][n+1];
+        ch = new int[n+1];
+        for(int i = 0; i < m; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph[a][b] = 1;
+        }
+        ch[1] = 1;
+        T.DFS(1);
+        System.out.println(answer);
+    }
+}
