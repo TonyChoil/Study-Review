92e975eb7444970a52a625eabae9d911632f134c
TONY2\creba <creba1234@gmail.com>
2024-01-17 16:27:02 +0900

    인프런 코테강의 08-02 완료

---
+++ Algorithm-test/src/inflearn/basicTest/ch8_ApplyingDFSandBFS/DFS2.java
@@ -0,0 +1,29 @@
+package inflearn.basicTest.ch8_ApplyingDFSandBFS;
+
+import java.util.*;
+class DFS2 {
+    static int answer=Integer.MIN_VALUE, c, n;
+    public void DFS(int L, int sum, int[] arr){
+        if(sum>c) return;
+        if(L==n){
+            answer=Math.max(answer, sum);
+        }
+        else{
+            DFS(L+1, sum+arr[L], arr);
+            DFS(L+1, sum, arr);
+        }
+    }
+
+    public static void main(String[] args){
+        DFS2 T = new DFS2();
+        Scanner kb = new Scanner(System.in);
+        c=kb.nextInt();
+        n=kb.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=kb.nextInt();
+        }
+        T.DFS(0, 0, arr);
+        System.out.println(answer);
+    }
+}
