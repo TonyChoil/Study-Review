64b014d7faf66c528f9087ea6bc43012a6e6992f
TONY2\creba <creba1234@gmail.com>
2024-01-24 14:00:01 +0900

    인프런 코테강의 08-10 완료

---
+++ Algorithm-test/src/inflearn/basicTest/ch8_ApplyingDFSandBFS/DFS10.java
@@ -0,0 +1,38 @@
+package inflearn.basicTest.ch8_ApplyingDFSandBFS;
+
+import java.util.Scanner;
+
+public class DFS10 {
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int answer = 0;
+    static int[][] board = new int[8][8];
+
+    public void DFS(int x, int y){
+        if(x == 7 && y == 7) answer++;
+        else{
+            for(int i = 0; i < 4; i++){
+                int nx = x+dx[i];
+                int ny = y+dy[i];
+                if(nx>=1 && nx <= 7 && ny>= 1 && ny <= 7 && board[nx][ny] == 0){
+                    board[nx][ny] = 1;
+                    DFS(nx, ny);
+                    board[nx][ny] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        DFS10 T = new DFS10();
+        Scanner sc = new Scanner(System.in);
+        for(int i = 1; i <= 7; i++){
+            for(int j = 1; j <= 7; j++){
+                board[i][j] = sc.nextInt();
+            }
+        }
+        board[1][1] = 1;
+        T.DFS(1, 1);
+        System.out.println(answer);
+    }
+}
